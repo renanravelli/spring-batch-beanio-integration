0ef89759c8453c5cbe8f1fe014aad3d47d3bdbc1
renanravelli <renanravellialves@gmail.com>
2020-02-16 17:51:37 -0300

    refactoring item reader file

--- src/main/java/br/com/renanravelli/batch/configuration/step/UserStepReaderFile.java
+++ src/main/java/br/com/renanravelli/batch/configuration/step/UserStepReaderFile.java
@@ -1,20 +1,48 @@
 package br.com.renanravelli.batch.configuration.step;
 
-import lombok.AllArgsConstructor;
+import br.com.renanravelli.batch.streams.enums.StreamName;
+import br.com.renanravelli.batch.streams.mapping.Registry;
+import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+import org.beanio.StreamFactory;
+import org.beanio.spring.BeanIOFlatFileItemReader;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.item.ExecutionContext;
 import org.springframework.batch.item.ItemReader;
 import org.springframework.batch.item.ItemWriter;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.FileSystemResource;
+
+import java.io.File;
 
 @Configuration
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class UserStepReaderFile {
 
-    private StepBuilderFactory stepBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
 
+    @Bean
+    @StepScope
+    @SneakyThrows
+    public ItemReader<Registry> userItemReaderFile(@Qualifier("streamFactory") StreamFactory streamFactory,
+                                                   @Value("${file.directory.out}") String path) {
+        BeanIOFlatFileItemReader<Registry> reader = new BeanIOFlatFileItemReader<>();
+        reader.setResource(new FileSystemResource(
+                path.concat(File.separator)
+                        .concat(StreamName.USER_CSV.getStream()
+                                .concat(StreamName.USER_CSV.getExtesion()))));
+        reader.setStreamName(StreamName.USER_CSV.getStream());
+        reader.setStreamFactory(streamFactory);
+        reader.open(new ExecutionContext());
+        reader.afterPropertiesSet();
+
+        return reader;
+    }
 
     /**
      * Step responsavel por realizar a execucao dos itens reader e writer.
