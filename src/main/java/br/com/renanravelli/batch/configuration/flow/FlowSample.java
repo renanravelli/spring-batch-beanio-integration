adef3574655d7409815caaa17a484f6fdcea7823
joao4018 <joao4018@gmail.com>
2020-03-24 13:53:36 -0300

    Criando exemplo de flow

---
+++ src/main/java/br/com/renanravelli/batch/configuration/flow/FlowSample.java
@@ -0,0 +1,26 @@
+package br.com.renanravelli.batch.configuration.flow;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.job.builder.FlowBuilder;
+import org.springframework.batch.core.job.flow.Flow;
+import org.springframework.batch.core.job.flow.support.SimpleFlow;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@RequiredArgsConstructor
+public class FlowSample {
+
+    @Bean("flow1")
+    public Flow flow1(@Qualifier("stepReaderUsers") Step stepReaderUsers,
+                      @Qualifier("stepReaderFileUsers") Step userStepReaderFile,
+                      @Qualifier("stepWriterUsersDB") Step stepWriterUsersBD) {
+        return new FlowBuilder<SimpleFlow>("flow1")
+                .start(stepReaderUsers)
+                .next(userStepReaderFile)
+                .next(stepWriterUsersBD)
+                .build();
+    }
+}
